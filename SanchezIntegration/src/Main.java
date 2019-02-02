31a3f913e82e2738b3c8506fda2dab34f67a5c9e
carlososanches <46728199+carlososanches@users.noreply.github.com>
2019-02-01 19:21:01 -0500

    This is my integration project
    Very new to GitHub, please grade this file!

--- SanchezIntegration/src/Main.java
+++ SanchezIntegration/src/Main.java
@@ -2,6 +2,19 @@ import java.util.Scanner;
 
 //Carlos Sanchez
 //A program integrating skills learned in COP 2006
+/*LIST OF JAVA BUILT IN DATA TYPES-
+ * BYTE-Minimum value of -128 and maximum value of 127. Useful for saving memory in large arrays, where memory saving actually matters.
+ * SHORT-This is 16bit it has a minimum value of -32,768 and a max of 32,767. same guidelines apply as byte, you can use a short to save memory in large array, in situations where memory actually matters!
+ * LONG- In Java SE 8 and further you can use the long data type to represent an unsigned  64-bit long, which has a minimum value of 0 and max of 2^64-1. Use when you need larger data types than that of int. 
+ * DOUBLE- double-precision 64-bit IEEE 754 floating point. For decimal values, this data type is generally the default choice. Never use for precise values like currency
+ * FLOAT - Single precision 32-bit IEEE 754 floating point. Use a float (instead of double) if you need to save memory in large arrays of floating point numbers. 
+ * INT-minimum value of -2^31 and maximum of value of 2^31-1. In Java SE 8 and further you can use the int data type to represent unasigneed 32-bit integer, which has a minimum value of 0 and max value of 2^32-1.
+ * CHAR-  Minimum value of '\u0000'(or0) and a maximum value of '\uffff' (or 65,535 inclusive)
+ * STRING- String objects are immutable, meaning once created, the value cannot be changed!
+The String class is not a primitive data type
+ * BOOLEAN- Typically used as a true or false statement, represents one bit of info and default value is false 
+ 
+ */
 public class Main {
 
 	
@@ -15,9 +28,32 @@ public class Main {
 
    public static void main(String[] args) {
 	   
-	   System.out.println("This is my Integration Project");
+	   boolean trueOrFalse = true;
+	   int wholeNumber = 2;
+	   double rationNumber = 2.0;
+	   String radical = "Awesome";
+	   
+	   if (trueOrFalse == true)
+	  System.out.println(wholeNumber + rationNumber + " " + radical); 
+	   
+	   //VARIABLE- A VARIABLE IS A LOCATION IN MEMORY THAT WE CAN ASSIGN VALUES TO.
+	   //SCOPE- A VARIABLE IS ONLY ACCESSIBLE IN THE METHOD IT WAS DECLARED.
+	   
+	   
+	   System.out.println("Greetings, this is my Integration Project");
 	   System.out.println("The Greatest project ever");
 
+	    final int fiveGuys = 5;
+	    
+	   // When a variable is declared with final keyword, its value can’t be modified. This means that you must initialize a final variable. 
+	    
+	    Object castedObject = "This will be casted";
+	    String bigString = (String)castedObject;
+	    
+	    //CASTING-  to make java to temporarily treat the variable like another data type.
+	    
+	    //THIS LAST PART IS MY ESCAPE SEQUENCE!
+	    System.out.println("This\nis\nawesome");
 	   
 	  	   
 	   
@@ -26,51 +62,7 @@ public class Main {
 	   
 	   
 	   
-	  	   //example of declaring a final
-	   final double SALES_TAX_RATE = .065;
-	   //SALEX_TAX_RATE = .075 Java will stop you from declaring this!!
-	   
-	   Scanner scan = new Scanner(System.in);
-	   System.out.println("Enter an integer");
-       int i = scan.nextInt();
-       scan.nextLine();
-       System.out.println("enter a string");
-       String s = scan.nextLine();
-       System.out.println("Enter a double");
-       double d = scan.nextDouble();
-      
-// any time going from integers to strings , clear buffer
-       System.out.println("String: " + s);
-       System.out.println("Double: " + d);
-       System.out.println("Int: " + i);
-	   
-	   
-       
-       
-      
-       
-       
-       
-       
-       
-       
-       
-       //IGNORE CODE
-      int int1 = 2;
-      double double1 = 2.0;
-      String string1 = "2";
-         
-     int int2 = 2;
-      double double2 = 2.0;
-      String string2 = "2";
-        /* Print the sum of both integer variables on a new line. */
-System.out.println("The result of + with ints is " + (int1 + int2));
-        /* Print the sum of the double variables on a new line. */
-System.out.println("The result of + with ints is " + (double1 + double2));
-
-        /* Concatenate and print the String variables on a new line. */
-System.out.println("The result of + with ints is " + (string1 + string2));
-	
+	  	
 
     } 
 }
